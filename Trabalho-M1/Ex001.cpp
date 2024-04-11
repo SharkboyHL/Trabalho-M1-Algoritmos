f1ea6983326c4873097da6929c27bb784bc5e670
SharkboyHL <104568918+SharkboyHL@users.noreply.github.com>
2024-04-11 23:41:16 +0000

    Questão 01 em andamento...

---
+++ Trabalho-M1/Ex001.cpp
@@ -0,0 +1,32 @@
+/*
+1. Quando observamos um objeto que se movimenta, notamos que a sua posição se modifica em função do tempo. Neste contexto, a equação horária da posição permite calcular a posição de um objeto em um dado instante de tempo t. Tal equação é descrita da seguinte forma:
+
+onde S(t), S0, v0 e a referem-se, respectivamente, a posição do objeto no tempo t (dado em segundos), posição inicial do objeto (dada em metros),  velocidade inicial do objeto (dada em metros por segundo) e aceleração do objeto (dada em metros por segundo ao quadrado). Crie um algoritmo para calcular a posição de um objeto em função do tempo conforme a equação horária da posição. 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Bem vindo a Calculadora da Função Horária da Posição" << endl;
+
+    cout << "Para começarmos insira ..." << endl;
+
+    float posiObject;
+    cout << "Posição Inicial: ";
+    cin >> posiObject;
+
+    float ;
+    cout << "";
+
+    float velocidadeInicial;
+    float aceleracao;
+
+
+
+    
+
+    return 0;
+}
