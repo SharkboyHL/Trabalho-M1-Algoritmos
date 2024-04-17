806abee38d6dcb59e100507f3951d7b8c38af4e5
SharkboyHL <104568918+SharkboyHL@users.noreply.github.com>
2024-04-17 15:34:05 +0000

    Ex008 em andamento...

--- Ex008.cpp
+++ Ex008.cpp
@@ -2,4 +2,30 @@
 8.  Crie um algoritmo que determina o tipo da raiz de uma equação do segundo grau ax² + bx + c = 0 (i.e., duas raízes reais distintas, uma raiz real ou uma raiz complexa).
 */
 
-#inc
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Insira os valores de a, b e c (inteiros)..." << endl;
+
+    int a, b, c;
+
+    cout << "Insira o valor de A: ";
+    cin >> a;
+
+    cout << "Insira o valor de B: ";
+    cin >> b;
+
+    cout << "Insira o valor de C: ";
+    cin >> c;
+
+    float delta = (b * b) - 4 * a * c;
+
+    float bhaskara = -b + (delta / delta) / 2 * a; 
+
+    cout << bhaskara << endl;
+
+    return 0;
+}
