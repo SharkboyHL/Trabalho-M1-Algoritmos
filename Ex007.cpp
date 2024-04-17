9e284fbe95114d85934ce164ad846071686ec0a5
SharkboyHL <104568918+SharkboyHL@users.noreply.github.com>
2024-04-17 13:22:38 +0000

    Ex007 concluído !

---
+++ Ex007.cpp
@@ -0,0 +1,48 @@
+/*
+7. Dado três ângulos, crie um algoritmo que determina se um triângulo  é acutângulo, retângulo ou obtusângulo.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Insira os angulos internos do triângulo..." << endl;
+    cout << "- Para ser acutângulo precisa que todos os ângulos sejam agudos ou seja menores que 90º." << endl;
+    cout << "- Para ser retângulo precisa que 1 ângulo seja reto ou seja que tenha 90º e o restante menores que 90º" << endl;
+    cout << "- Para ser escaleno precisa que todos os lados tenham medidas diferentes." << endl;
+
+
+    float anguloA, anguloB, anguloC;
+
+    cout << endl << "Ângulo A: ";
+    cin >> anguloA;
+
+    cout << "Ângulo B: ";
+    cin >> anguloB;
+
+    cout << "Ângulo C: ";
+    cin >> anguloC;
+
+    if(anguloA > 0 && anguloB > 0 && anguloC > 0 && (anguloA + anguloB + anguloC == 180)){
+        if(anguloA < 90 && anguloB < 90 && anguloC < 90){
+            cout << endl << "É um Triângulo Acutângulo." << endl;
+        }else if(anguloA == 90 && anguloB < 90 && anguloC < 90){
+            cout << endl << "É um Triângulo Retângulo." << endl;
+        }else if(anguloB == 90 && anguloA < 90 && anguloC < 90){
+            cout << endl << "É um Triângulo Retângulo." << endl;
+        }else if(anguloC == 90 && anguloA < 90 && anguloB < 90){
+            cout << endl << "É um Triângulo Retângulo." << endl;
+        }else if(anguloA > 90 && anguloB < 90 && anguloC < 90){
+            cout << endl << "É um Triângulo Obtusângulo." << endl;
+        }else if(anguloB > 90 && anguloA < 90 && anguloC < 90){
+            cout << endl << "É um Triângulo Obtusângulo." << endl;
+        }else if(anguloC > 90 && anguloA < 90 && anguloB < 90){
+            cout << endl << "É um Triângulo Obtusângulo." << endl;
+        }
+    }else{
+        cout << endl << "VALORES INSERIDOS INVÁLIDOS!" << endl;
+    }
+    return 0;
+}
